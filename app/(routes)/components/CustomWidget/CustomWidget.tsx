a289a5557def4ee02b2ce44aa8f39c67aeb95c4b
RockBanzai <hrocca76@gmail.com>
2024-09-14 18:38:39 -0300

    feat: Add dynamic content rendering to CustomWidget component

--- app/(routes)/components/CustomWidget/CustomWidget.tsx
+++ app/(routes)/components/CustomWidget/CustomWidget.tsx
@@ -1,16 +1,22 @@
 import CustomIcon from "@/components/CustomIcon";
 import { Building } from "lucide-react";
+import { ReactNode } from "react";
 
+interface CustomWidgetProps {
+  content: ReactNode;
+  title?: string;
+  icon?: React.ElementType;
+}
 
-export default function CustomWidget() {
+export default function CustomWidget({ content, title = "Last News Market", icon = Building }: CustomWidgetProps) {
     return (
         <div className="shadow-sm bg-background rounded-lg p-5">
-            <div className="flex gap-x-2 items-center">
-                <CustomIcon icon={Building} />
-                <p className="text-xl">Last News Market</p>
+            <div className="flex gap-x-2 items-center mb-4">
+                <CustomIcon icon={icon} />
+                <p className="text-xl">{title}</p>
             </div>
             <div>
-                
+                {content}
             </div>
         </div>
     )
