121292ff94d44f46d35e88483da4abecd2e9f53a
RockBanzai <hrocca76@gmail.com>
2024-10-05 11:37:33 -0300

    replace mock data by service api and transformation data from origin.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/news/:path*',
+        destination: 'http://207.244.247.95:8000/news/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
